93848f95c3b7ca8b08c458d1651a6453a32c4b9a
Peter Toth <peter.toth@eon-hungaria.com>
2021-02-26 21:37:06 +0100

    End.cs added

---
+++ End.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpTools
+{
+    class End : Stage
+    {
+        public End(string name) : base(name, "End") { }
+        public End() : this("End") { }
+        public OutputParameterCollection OutputParameters { get; set; } = new OutputParameterCollection();
+
+
+    }
+}
