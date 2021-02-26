c82f345c2fa9efd46bf690cb668eb9f1e086d167
Peter Toth <peter.toth@eon-hungaria.com>
2021-02-26 12:38:55 +0100

    SubSheetInfo has been put into Page as a Property.

--- Page.cs
+++ Page.cs
@@ -11,21 +11,22 @@ namespace BpTools
     class Page : BpElement
     {
         public string StageId { get; set; } = System.Guid.NewGuid().ToString();
-        public string Type { get; set; } = "normal";
+        public string Type { get; set; } = "Normal";
         public bool Published { get; set; } = false;
         public string Name { get; set; } = "";
         public View View { get; set; } = new View();
 
-        public Collection<Stage> Stages { get; } = new Collection<Stage>();
+        public SubSheetInfo SubSheetInfo { get; set; }
+        public StageCollection Stages { get; } = new StageCollection();
 
         public Page(string name)
         {
             Name = name;
+            SubSheetInfo = new SubSheetInfo(Name);
         }
 
-        public Page (string name, bool published)
+        public Page (string name, bool published) : this (name)
         {
-            Name = name;
             Published = published;
         }
     }
