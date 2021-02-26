d6c957d5115b476d75fc7d7c76d1168c9e5a16e9
Peter Toth <peter.toth@eon-hungaria.com>
2021-02-26 21:39:21 +0100

    SubsheetInfo added

---
+++ SubsheetInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpTools
+{
+    class SubSheetInfo : Stage
+    {
+        public SubSheetInfo(string name) : base(name, "SubSheetInfo")
+        {
+            DisplayX = -270;
+            DisplayY = 30;
+            DisplayWidth = 150;
+            DisplayHeight = 90;
+        }
+    }
+}
