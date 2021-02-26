e8309ddbe9e59c5a03e78282ad11c73578bc403f
Peter Toth <peter.toth@eon-hungaria.com>
2021-02-26 21:39:41 +0100

    XmlFragmentWriter added

---
+++ XmlFragmentWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BpTools
+{
+    class XmlFragmentWriter : XmlTextWriter
+    {
+        public XmlFragmentWriter(TextWriter writer) : base (writer)
+        {
+
+        }
+
+        override public void WriteStartDocument()
+        {
+
+        }
+
+        override public void WriteStartDocument(bool standalone)
+        {
+
+        }
+    }
+}
