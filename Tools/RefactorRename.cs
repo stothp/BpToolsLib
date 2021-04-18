36c1c889ef050508db899ec8ee5cbdd1e9566355
stothp <stothp@gmail.com>
2021-04-18 11:03:17 +0200

    RefactorRename added

---
+++ Tools/RefactorRename.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BpToolsLib.Tools
+{
+    public static class RenameStage
+    {
+
+        private static void RenameDataReferences(StageSet stageSet, string originalName, string newName)
+        {
+            foreach (Stage bpStage in stageSet)
+            {
+                if (bpStage is IExpressionHolder)
+                {
+                    foreach (MutableExpression expression in ((IExpressionHolder)bpStage).Expressions)
+                    {
+                        expression.Value = expression.Value.Replace("[" + originalName + "]", "[" + newName + "]");
+                    }
+                }
+                if (bpStage is IDataNameHolder)
+                {
+                    foreach (MutableDataName dataName in ((IDataNameHolder)bpStage).DataNames)
+                    {
+                        if (dataName.Value.Equals(originalName))
+                        {
+                            dataName.Value = newName;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void RenameCollectionReferences(StageSet stageSet, string originalName, string newName)
+        {
+            foreach (Stage bpStage in stageSet)
+            {
+                if (bpStage is IExpressionHolder)
+                {
+                    foreach (MutableExpression expression in ((IExpressionHolder)bpStage).Expressions)
+                    {
+                         expression.Value = expression.Value.Replace("[" + originalName + ".", "[" + newName + ".");
+                    }
+                }
+                if (bpStage is IDataNameHolder)
+                {
+                    foreach (MutableDataName dataName in ((IDataNameHolder)bpStage).DataNames)
+                    {
+                        if (dataName.Value.StartsWith(originalName + "."))
+                        {
+                            dataName.Value = Regex.Replace(dataName.Value, "^" + originalName + ".", newName + ".");
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Rename(Stage stage, StageSet stageSet, string newName)
+        {            
+            string originalName = stage.Name;
+            stage.Name = newName;
+
+            if (stage is StageData)
+            {
+                RenameDataReferences(stageSet, originalName, newName);
+            }
+            if (stage is StageCollection)
+            {
+                RenameDataReferences(stageSet, originalName, newName);
+                RenameCollectionReferences(stageSet, originalName, newName);
+            }
+        }
+
+    }
+}
